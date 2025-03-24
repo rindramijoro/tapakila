6d3491fdcdc9c6f30c6b39c0cea5d0f3f3f6772e
rindramijoro <rindramijoro@gmail.com>
2025-03-24 13:13:01 +0300

    fix: changing the icons of every section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import {Admin, EditGuesser, ListGuesser, Resource, ShowGuesser} from "react-admin"
-import dataProvider from "./dataProvider";//+
+import dataProvider from "./dataProvider";
+import {
+  FaCalendar,
+  FaUser,
+  FaTicketAlt,
+  FaClipboardList,
+} from "react-icons/fa";
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
@@ -8,24 +14,28 @@ export const App = () => (
       list={ListGuesser}
       edit={EditGuesser}
       show={ShowGuesser}
+      icon={FaCalendar}
     />
     <Resource
       name="users"
       list={ListGuesser}
       edit={EditGuesser}
       show={ShowGuesser}
+      icon={FaUser}
     />
     <Resource
       name="tickets"
       list={ListGuesser}
       edit={EditGuesser}
       show={ShowGuesser}
+      icon={FaTicketAlt}
     />
     <Resource
       name="reservations"
       list={ListGuesser}
       edit={EditGuesser}
       show={ShowGuesser}
+      icon={FaClipboardList}
     />
   </Admin>
 );
